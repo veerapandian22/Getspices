0921288b703276f80bec23e873b24b48e03f82cb
veerapandian <veerapandian_r@homedepot.com>
2023-09-21 12:17:21 +0530

    about & contact page

--- vaa_ui/src/app/app.module.ts
+++ vaa_ui/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ContactComponent,
+    AboutComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
