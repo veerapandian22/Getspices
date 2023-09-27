01fee7704df7ceee8aeb99e7dd879becfb991ac9
veerapandian <veerapandian_r@homedepot.com>
2023-09-28 03:23:48 +0530

    fix

--- vaa_api/users/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Users
-
-class UsersSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Users # this is the model that is being serialized
-        fields = ('name', 'email', 'ph_no', 'added_on')
--- vaa_api/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import users
-
-urlpatterns = [
-    path('users/', users, name="users")
-]
